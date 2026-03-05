c7a05bc359f6ae3c03ab3eca860fee974ffd66b9
kangmin1164 <kangmin1164@gmail.com>
2026-03-05 12:32:50 +0900

    최종 버전

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Control;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(55, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(234, 271);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("한컴 고딕", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(350, 178);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 25);
+            label1.TabIndex = 1;
+            label1.Text = "kangmin1164@gmail.com";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("한컴 고딕", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.ForeColor = Color.LightSeaGreen;
+            label2.Location = new Point(350, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 25);
+            label2.TabIndex = 2;
+            label2.Text = "컴퓨터SW";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("한컴 고딕", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label3.Location = new Point(350, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 35);
+            label3.TabIndex = 3;
+            label3.Text = "강민규";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("한컴 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(350, 224);
+            button1.Name = "button1";
+            button1.Size = new Size(244, 31);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Menu;
+            button2.Font = new Font("한컴 고딕", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.Location = new Point(438, 276);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "GitHub";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Font = new Font("한컴 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.Location = new Point(128, 305);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 29);
+            button3.TabIndex = 6;
+            button3.Text = "사진 변경";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(640, 356);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,43 @@ namespace MyBusinessCard
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            // 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+    {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "https://github.com/kangmin1164-cmd",
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+    {
+                MessageBox.Show("오류가 발생했습니다: " + ex.Message);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.images;
+        }
     }
 }
